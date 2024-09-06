8be6f6f884e3c7da2dbeee3e8a48fff678f050cb
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-09-07 02:45:07 +0330

    add: automation.sh for implement end to end automation process.

---
+++ automation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Apply Terraform configuration
+echo "Applying Terraform configuration..."
+terraform apply -auto-approve
+
+# Wait for Terraform to complete
+echo "Waiting for Terraform to finalize..."
+sleep 10
+
+# Process Terraform output using Python script
+echo "Processing Terraform output..."
+terraform output -json | python3 automation.py
+
+# Wait for the Python script to complete
+echo "Waiting for the Python script to complete..."
+sleep 10
+
+# Change to the Ansible directory
+echo "Navigating to the Ansible directory..."
+cd ansible
+
+# Run Ansible playbook
+echo "Running Ansible preparing playbook..."
+sudo ansible-playbook -i inventory/inventory.ini playbooks/preparing.yml
+
+echo "Waiting for the before ansible playbook to complete..."
+sleep 5
+
+# Run Ansible playbook
+echo "Running Ansible docker playbook..."
+sudo ansible-playbook -i inventory/inventory.ini playbooks/docker.yml

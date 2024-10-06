a7a0188bf740c3b378258d1d3b964351c2e4f078
siavash mohseni <siavashmohsenimhi79@gmail.com>
2024-10-06 19:15:27 +0330

    add: kubernetes metrics-server resource and manifest.

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -48,54 +48,12 @@ resource "null_resource" "cert_manager" {
   ]
 }
 
-resource "kubectl_manifest" "clusterissuer" {
-  yaml_body = file("./cert-manager/clusterissuer.yml") 
-
-  depends_on = [
-    null_resource.cert_manager
-  ] 
-}
-
 resource "kubectl_manifest" "metrics_server" {
-  yaml_body = file("./metric-server/metrics-server-components.yaml")
+  yaml_body = file("./metrics-server/components.yaml")
   
   depends_on = [
-    kubectl_manifest.clusterissuer,
     null_resource.cert_manager,
     null_resource.cert_manager_repo,
     helm_release.nginx_ingress
-  ]
-}
-
-resource "time_sleep" "wait_180_seconds" {
-  create_duration = "180s"
-}
-
-resource "kubernetes_manifest" "metrics_server_patch" {
-  manifest = {
-    "apiVersion" = "apps/v1"
-    "kind"       = "Deployment"
-    "metadata" = {
-      "name"      = "metrics-server"
-      "namespace" = "kube-system"
-    }
-    "spec" = {
-      "template" = {
-        "spec" = {
-          "containers" = [{
-            "name" = "metrics-server"
-            "args" = [
-              "--kubelet-insecure-tls",
-              "--authorization-always-allow-paths=/livez,/readyz"
-            ]
-          }]
-        }
-      }
-    }
-  }
-
-  depends_on = [
-    kubectl_manifest.metrics_server,
-    time_sleep.wait_180_seconds
   ]
 }
